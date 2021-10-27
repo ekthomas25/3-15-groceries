84c1bf74d227618b43d1045ea600f71fa9564641
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-10-26 21:18:29 -0700

    BUGFIX: Add ul tags to HTML so that the list shows up, move h1 inside of container.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function blankField(text) {
+  if (text === "") {
+
+  }
+}
+
+
+$(document).ready(function() {
+  $("#grocery-form").submit(function(event) {
+    event.preventDefault();
+
+    const input1 = $("#item1").val();
+    const input2 = $("#item2").val();
+    const input3 = $("#item3").val();
+    const input4 = $("#item4").val();
+    const input5 = $("#item5").val();
+    const input6 = $("#item6").val();
+
+    let listArray = [input1, input2, input3, input4, input5, input6];
+
+    let orderedArray = listArray.sort();
+
+    orderedArray.forEach(function(element) {
+      $("ul").append("<li>" + element + "</li>");
+    });
+  });
+});
+
