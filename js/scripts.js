e4daa07285f649b556e092bb744928a51bc5d3ba
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-10-26 22:22:33 -0700

    Add toUpperCase() to each value.

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
   $("#grocery-form").submit(function(event) {
     event.preventDefault();
 
-    const input1 = $("#item1").val();
-    const input2 = $("#item2").val();
-    const input3 = $("#item3").val();
-    const input4 = $("#item4").val();
-    const input5 = $("#item5").val();
-    const input6 = $("#item6").val();
+    const input1 = $("#item1").val().toUpperCase();
+    const input2 = $("#item2").val().toUpperCase();
+    const input3 = $("#item3").val().toUpperCase();
+    const input4 = $("#item4").val().toUpperCase();
+    const input5 = $("#item5").val().toUpperCase();
+    const input6 = $("#item6").val().toUpperCase();
 
     let listArray = [input1, input2, input3, input4, input5, input6];
 
@@ -24,6 +24,7 @@ $(document).ready(function() {
       if (element.trim().length === 0) {
         return null;
       } $("ul").append("<li>" + element + "</li>");
+      $(".container").hide();
     });
   });
 });
